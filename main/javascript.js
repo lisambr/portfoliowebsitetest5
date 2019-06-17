9f2485c74d65d4708c2d7016c28efbd5b929a108
Lisa Marie Becht Rojas <lmbr@Lisas-MacBook.local>
2019-06-17 17:07:35 +0200

    start of js testing

---
+++ main/javascript.js
@@ -0,0 +1,15 @@
+//HERO//
+const HERO = document.querySelector("#bird");
+
+
+//04JUMP ONE TIME//
+
+const ONEJUMPBUTTON = document.querySelector("#btn04");
+console.log (ONEJUMPBUTTON);
+ONEJUMPBUTTON.addEventListener("click",onejump);
+
+function onejump(){
+    HERO.classList = "";
+    HERO.classList.add("onejump");
+    console.log(STARTONEJUMP);
+}
