abc3b6d03d06f2d4cd6a56f90b12fdcbb2d90fab
Lisa Marie Becht Rojas <lmbr@Lisas-MacBook.local>
2019-06-17 17:22:33 +0200

    more test for navbar

--- main/javascript.js
+++ main/javascript.js
@@ -13,3 +13,18 @@ function onejump(){
     HERO.classList.add("onejump");
     console.log(STARTONEJUMP);
 }
+
+//NAVBAR//
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
